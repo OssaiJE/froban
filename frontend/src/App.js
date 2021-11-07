a12667288d63b7f999ec71aa15c1967a5dc28820
Julius Ossai <ossaije1@gmail.com>
2021-11-07 15:20:07 +0100

    updated again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-
+import { Fragment } from "react";
+import {Container, } from 'react-bootstrap'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
-    
   return (
-    <>
-      <h1>Welcome to Froban stores</h1>
-    </>
+    <Fragment>
+      <Header />
+      <main>
+          <Container>
+        <h1>Welcome to Froban stores</h1>
+        </Container>
+      </main>
+      <Footer />
+    </Fragment>
   );
-}
+};
 
 export default App;
