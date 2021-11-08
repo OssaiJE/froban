ab9ed132a3485269f5d462c41557d211d844605a
OssaiJE <ossaije1@gmail.com>
2021-11-08 03:31:19 +0100

    worked on header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <Fragment>
       <Header />
-      <main>
+      <main className="py-3">
         <Container>
           <h1>Welcome to Froban stores!</h1>
         </Container>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,18 +4,30 @@ import { Container, Navbar, Nav } from "react-bootstrap";
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-        <Container fluid>
+      {/* Try sticky=top if fixed=top doesn't work */}
+      <Navbar
+        bg="dark"
+        variant="dark"
+        expand="lg"
+        sticky="top"
+        collapseOnSelect
+      >
+        <Container>
           <Navbar.Brand href="/">Froban Stores</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
-              className="me-auto my-2 my-lg-0"
+              className="ml-auto my-2 my-lg-0"
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Nav.Link href="/cart">Cart</Nav.Link>
-              <Nav.Link href="/signin">Sign In</Nav.Link>
+              <Nav.Link href="/cart">
+                {" "}
+                <i className="fas fa-shopping-cart"></i> Cart
+              </Nav.Link>
+              <Nav.Link href="/signin">
+                <i className="fas fa-user"></i>Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
